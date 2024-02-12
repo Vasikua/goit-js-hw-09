2f974cf91405f736ba5d5545760b9f929c1f0582
Vasyl Mandiak <Vasik_ua@ukr.net>
2024-02-11 22:29:52 -0600

    all done

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+
+function readFormData() {
+   let email = form.email.value.trim();
+   let message = form.message.value.trim();
+  return {
+        email,
+      message
+  };
+}
+
+form.addEventListener('input', (event) => {
+    const data = readFormData(event.currentTarget);
+  if(form.email.value && form.message.value){
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(STORAGE_KEY, jsonData);
+  }
+})
+
+
+ try { 
+    const rowData = localStorage.getItem(STORAGE_KEY);
+    const data = JSON.parse(rowData);    
+    form.email.value = data.email;
+    form.message.value = data.message;
+ }
+ catch (error) {
+     console.log('Local storage is empty')
+}  
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let email = form.email.value;
+   let message = form.message.value;
+  if (email && message) {
+        console.log({email, message});
+        localStorage.removeItem(STORAGE_KEY);
+        form.reset();
+  }
+})
